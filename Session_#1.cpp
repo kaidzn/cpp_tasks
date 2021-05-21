458fb3d287f922be2e266186c27241563877248f
kaidzn <83287458+kaidzn@users.noreply.github.com>
2021-05-21 19:16:51 +0600

    Create Session_#1.cpp

---
+++ Session_#1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[50][50], n, m, even[50];
+     cin >> n >> m ;
+     for(int i = 0 ; i < n ; i++){
+         for(int j = 0 ; j < m ; j++){
+           cin >> a[i][j];
+     }
+     }
+     for(int i = 0 ; i < n ; i++){
+       for(int j = 0; j < n; j++){
+       cout << a[i][j]<<" ";
+       }
+       cout << endl;
+     }
+     int count=0;
+     for(int i = 0 ; i < n ; i++){
+         even[i] = a[i][0];
+         for(int j = 0 ; j < m ; j++){
+             if(a[i][j]%2 == 0){
+                 even[i] = a[i][j];
+                 count+=1;
+             }
+         }
+        cout <<"even numbers of "<< i + 1 <<"-rows : "<< count << endl;
+        count=0;
+         
+     }
+    
+    
+     }
