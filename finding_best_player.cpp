385ec5bf40ef9826fc9f7105e1f06332ed2079bf
kaidzn <83287458+kaidzn@users.noreply.github.com>
2021-11-01 22:27:31 +0600

    Create finding_best_player.cpp

---
+++ finding_best_player.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct Players{
+  string name;
+  string surname;
+  string position;
+  int quantity_of_games;
+  int quantity_of_goals;
+  
+}players[5];
+void BestStriker(int n){
+  for(int i = 0; i < n; i++){
+    if(players[i].quantity_of_goals > players[i + 1].quantity_of_goals){
+     int temp = players[i].quantity_of_goals;
+     players[i].quantity_of_goals = players[i + 1].quantity_of_goals;
+     players[i].quantity_of_goals = temp;
+    }
+  }
+  // cout <<"it is sort function";
+}
+int main (){
+  int n;
+  
+  cout <<"Enter the quantity of players: ";
+  cin >> n;
+  cout << "Input "<<endl;
+ 
+  for(int i = 0; i < n; i++){
+    cin >> players[i].name;
+    cin >> players[i].surname;
+    cin >> players[i].position;
+    cin >> players[i].quantity_of_games;
+    cin >> players[i].quantity_of_goals;
+  }
+  BestStriker(n);
+  cout <<"Best player is: "<<endl;
+  for(int i = 0; i < 1; i++){
+    cout << players[i].name<<" "<<players[i].surname<<" "<<players[i].position<<" "<<players[i].quantity_of_games<<" "<<players[i].quantity_of_goals<<endl;
+  }
+  
+}
+  
+
+
